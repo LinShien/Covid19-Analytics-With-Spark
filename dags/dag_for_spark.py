ce982a91ba5a47eb9ca9348dc9d598158c4e0e1d
size7311 <size7311@gmail.com>
2020-06-23 22:32:23 +0800

    Add task for downloading data

--- dags/dag_for_spark.py
+++ dags/dag_for_spark.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 
 from airflow import DAG
+from airflow.models import Variable
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.contrib.sensors.file_sensor import FileSensor
 
 import pandas as pd
 
@@ -13,10 +15,20 @@ default_args = {
         'schedule_interval' : '@daily'
 }
 
-bash_command = '''
+download_command = '''
+    cd ~/Covid19-Analytics-With-Spark/data
+    wget -O "COVID-19 Activity.csv" '''
+
+spark_command = '''
     cd ~/Covid19-Analytics-With-Spark
     spark-submit sparkLoader.py
 '''
+
+tearDown_command = '''
+    cd ~/Covid19-Analytics-With-Spark/data
+    rm -f "COVID-19 Activity.csv"
+'''
+    
 
 def readSparkResults(**context):
     dataframe = pd.read_csv('/home/size7311/Covid19-Analytics-With-Spark/results.csv')
@@ -27,16 +39,33 @@ def readSparkResults(**context):
 
 
 with DAG('dag_for_spark', catchup = False, default_args = default_args) as dag:
-    sparkOperator = BashOperator(
+    downloadDataTask = BashOperator(
+	    task_id = 'download_data_task',
+	    bash_command = download_command + Variable.get('covid19_data_url')
+	    )
+    
+    checkDataExists = FileSensor(
+	    task_id = 'check_data_task',
+	    filepath = '/home/size7311/Covid19-Analytics-With-Spark/data/COVID-19 Activity.csv',
+	    poke_interval = 10,
+	    timeout = 100
+	    )
+
+    sparkTask = BashOperator(
             task_id = 'spark_task',
-            bash_command = bash_command,
+            bash_command = spark_command
             )
     
-    pandasOperator = PythonOperator(
+    pandasTask = PythonOperator(
 	    task_id = 'pandas_task',
-   	    python_callable = readSparkResults,
+   	    python_callable = readSparkResults
     )
 
-    sparkOperator >> pandasOperator
+    tearDownTask = BashOperator(
+	    task_id = 'tear_down_task',
+	    bash_command = tearDown_command
+    )
+
+    downloadDataTask >> checkDataExists >> sparkTask >> pandasTask >> tearDownTask
 	    
 
